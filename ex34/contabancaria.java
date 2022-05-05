c8881e09567b6cd744fd4520997659e5b291a15c
Nickolas Kenji <101301519+NickolasKenji@users.noreply.github.com>
2022-05-05 20:42:00 -0300

    contabancaria.java

---
+++ ex34/contabancaria.java
@@ -0,0 +1,17 @@
+package ex34;
+
+public class contabancaria {
+	public String agencia;
+	public String numero;
+	public double saldo;
+	
+	contabancaria(){
+		
+	}
+	
+	contabancaria(String ag, String num, double sal){
+		this.agencia=ag;
+		this.numero=num;
+		this.saldo=sal;
+	}
+}
